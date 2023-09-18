c8827bbf00efdeabfe6970fc8b24cb86792fc198
LucianoLupo95 <lnlupo95@gmail.com>
2023-09-18 19:46:58 -0300

    Clase 04

---
+++ script.js
@@ -0,0 +1,13 @@
+// let boton = document.querySelector("#toggle-dark-mode");
+// let body = document.querySelector("body");
+// let darkMode = false;
+// boton.addEventListener("click",()=>{
+//     if (darkMode == false){
+//         darkMode = true;
+//         body.style.backgroundColor = "black";
+//     }else{
+//         darkMode = false;
+//         body.style.backgroundColor = "white";
+//     }
+// })
+<button id="toggle-dark-mode">Apretame</button>
